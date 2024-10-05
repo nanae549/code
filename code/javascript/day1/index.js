5ac154dcff3442be25990d7a8253364f9a72c63c
nanae joram <joramyan804@gmail.com>
2024-10-05 23:30:39 +0530

    Add files via upload

---
+++ code/javascript/day1/index.js
@@ -0,0 +1,43 @@
+console.log('im a good boy');
+console.log(65);
+console.log('im a good boy',65);
+console.log(45,78);
+console.log(98,'raman');
+console.log(23+76);
+console.log(63*67);
+console.log(54-32);
+console.log(87/3);
+console.log(74+'34');
+console.log(74-'34');
+console.log(74*'34');
+console.log(74/'34');
+console.log(74%'34');
+console.log('hem','rem');
+console.log('hem'+'rem');
+console.log(12**3);//it is used for power
+console.log(12+true);
+console.log(12+false);
+console.log(0+true);
+console.log(true+true);
+console.log(true+null);
+console.log(+null);
+console.log(true);
+console.log(+true);
+
+//declaring variable
+//there are 3 types of variables
+//var,const,let
+// var name= 'ram'
+// console.log(name);
+// console.log(typeof name);
+var x= 54
+console.log(x);
+console.log(typeof x);
+
+
+
+
+
+
+
+
